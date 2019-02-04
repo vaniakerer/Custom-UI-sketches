2580ef25312d090c8d4d4a4f6b2d2443249e555c
ivan <ivan.kerer.work@gmail.com>
2019-02-04 23:54:36 +0200

    Add changing checked on click

--- app/src/main/java/com/example/ivan/customviews/anim_check_box/AnimCheckBox.kt
+++ app/src/main/java/com/example/ivan/customviews/anim_check_box/AnimCheckBox.kt
@@ -2,9 +2,7 @@ package com.example.ivan.customviews.anim_check_box
 
 import android.animation.ValueAnimator
 import android.content.Context
-import android.graphics.Canvas
-import android.graphics.Color
-import android.graphics.Paint
+import android.graphics.*
 import android.util.AttributeSet
 import android.view.View
 
@@ -14,23 +12,46 @@ class AnimCheckBox @JvmOverloads constructor(
         defStyleAttr: Int = 0
 ) : View(context, attrs, defStyleAttr) {
 
-    private var isCkecked = false
+    private var isChecked = false
 
     private var checkedColor = Color.CYAN
     private var unCheckedColor = Color.GRAY
 
     private var size: Float = 150F
     private var radus: Float = 75F
+    private var strokeWidth = 60F
 
     private val checkedPaint = Paint()
     private val unCheckedPaint = Paint()
 
+    private val clippedArea = Path()
+    private val fullArea = Path()
+    private val clipRegion = Region()
+
     private var checkOffset = 0F //0..1
+    private val startDrawcheckedFalseBorderOffset = 0.3F
 
     private var checkedChangeValueAnimator: ValueAnimator? = null
 
+    init {
+        setOnClickListener { setChecked(!isChecked) }
+    }
+
     override fun onDraw(canvas: Canvas?) {
-        canvas?.drawCircle(radus, radus, radus * checkOffset, if (isCkecked) checkedPaint else unCheckedPaint)
+
+        if (checkOffset <= startDrawcheckedFalseBorderOffset) {
+            val clipedPathRadius = getUncheckedOffset(checkOffset) * strokeWidth
+            clippedArea.addCircle(radus, radus, clipedPathRadius, Path.Direction.CW)
+            canvas?.drawCircle(radus, radus, radus, unCheckedPaint)
+            canvas?.clipPath(clippedArea, Region.Op.REPLACE)
+        } else {
+            canvas?.drawCircle(
+                    radus,
+                    radus,
+                    radus * checkOffset,
+                    checkedPaint
+            )
+        }
     }
 
     override fun onAttachedToWindow() {
@@ -54,13 +75,17 @@ class AnimCheckBox @JvmOverloads constructor(
         if (checked) {
             checkedChangeValueAnimator = ValueAnimator.ofFloat(checkOffset, 1F)
         } else {
-            checkedChangeValueAnimator = ValueAnimator.ofFloat(1F, checkOffset)
+            checkedChangeValueAnimator = ValueAnimator.ofFloat(checkOffset, 0F)
         }
 
         checkedChangeValueAnimator?.addUpdateListener {
             checkOffset = it.animatedValue as Float
             invalidate()
         }
+
+        checkedChangeValueAnimator?.duration = 3000
+
+        this.isChecked = checked
 
         checkedChangeValueAnimator?.start()
 
@@ -78,11 +103,20 @@ class AnimCheckBox @JvmOverloads constructor(
         }
     }
 
+    private fun getUncheckedOffset(currentOffset: Float): Float {
+        if (currentOffset > startDrawcheckedFalseBorderOffset)
+            throw IllegalStateException("offset should be lower than $startDrawcheckedFalseBorderOffset")
+
+        return 1 - startDrawcheckedFalseBorderOffset * (1F - currentOffset) / 100
+    }
+
     private fun initPaints() {
         checkedPaint.color = checkedColor
         checkedPaint.isAntiAlias = true
 
         unCheckedPaint.color = unCheckedColor
         unCheckedPaint.isAntiAlias = true
+
+        fullArea.addCircle(radus, radus, radus, Path.Direction.CW)
     }
 }
