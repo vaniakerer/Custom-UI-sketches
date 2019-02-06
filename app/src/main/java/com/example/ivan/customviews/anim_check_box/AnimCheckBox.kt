c21452132eb223c3edec1bb1415c6e4a7fc3759e
ivan <ivan.kerer.work@gmail.com>
2019-02-07 01:10:55 +0200

    add drawing check icon

--- app/src/main/java/com/example/ivan/customviews/anim_check_box/AnimCheckBox.kt
+++ app/src/main/java/com/example/ivan/customviews/anim_check_box/AnimCheckBox.kt
@@ -1,11 +1,12 @@
 package com.example.ivan.customviews.anim_check_box
 
+import android.animation.Animator
 import android.animation.ValueAnimator
 import android.content.Context
 import android.graphics.*
 import android.util.AttributeSet
-import android.util.Log
 import android.view.View
+import android.graphics.DashPathEffect
 
 
 class AnimCheckBox @JvmOverloads constructor(
@@ -17,33 +18,44 @@ class AnimCheckBox @JvmOverloads constructor(
     private var isChecked = false
 
     private var checkedColor = Color.CYAN
-    private var unCheckedColor = Color.GRAY
+    private var unCheckedColor = Color.BLUE
+    private var checkIconColor = Color.WHITE
 
     private var radius: Float = 0F
     private var strokeWidth = 8F //todo hardcoded for xxhdpi
 
     private val checkedPaint = Paint()
     private val borderPaint = Paint()
+    private val checkIconPaint = Paint()
 
     private val fullArea = Path()
+
+    private val checkIconPath = Path()
+    private val checIconPathMeasure = PathMeasure()
 
     private val centerPoint = PointF()
 
     private var checkOffset = 0F //0..1
 
+    private var checkIconDrawOffset = 0F
+
     private var checkedChangeValueAnimator: ValueAnimator? = null
+    private var checkIconDrawOffsetValueAnimator: ValueAnimator? = null
 
     init {
         setOnClickListener { setChecked(!isChecked) }
     }
 
     override fun onDraw(canvas: Canvas?) {
+        if (canvas == null) return
+
         drawCircleBackground(canvas)
         drawBorder(canvas)
+        drawCheckIconPath(canvas)
     }
 
-    private fun drawCircleBackground(canvas: Canvas?) {
-        canvas?.drawCircle(
+    private fun drawCircleBackground(canvas: Canvas) {
+        canvas.drawCircle(
                 centerPoint.x,
                 centerPoint.y,
                 radius * checkOffset,
@@ -51,8 +63,15 @@ class AnimCheckBox @JvmOverloads constructor(
         )
     }
 
-    private fun drawBorder(canvas: Canvas?) {
-        canvas?.drawCircle(centerPoint.x, centerPoint.y, radius, borderPaint)
+    private fun drawBorder(canvas: Canvas) {
+        canvas.drawCircle(centerPoint.x, centerPoint.y, radius, borderPaint)
+    }
+
+    private fun drawCheckIconPath(canvas: Canvas) {
+        val pathLength = checIconPathMeasure.length
+        checkIconPaint.pathEffect = DashPathEffect(floatArrayOf(pathLength, pathLength),
+                Math.max(checkIconDrawOffset * pathLength, checkIconDrawOffset))
+        canvas.drawPath(checkIconPath, checkIconPaint)
     }
 
     override fun onAttachedToWindow() {
@@ -75,27 +94,51 @@ class AnimCheckBox @JvmOverloads constructor(
         radius = w / 2 - strokeWidth / 2
         centerPoint.x = (w / 2).toFloat()
         centerPoint.y = (h / 2).toFloat()
+
+        initCheckIconPath()
     }
 
     fun setChecked(checked: Boolean) {
-        checkedChangeValueAnimator?.cancel()
+        this.isChecked = checked
+        startBackgroundAnimation()
+    }
 
-        if (checked) {
-            checkedChangeValueAnimator = ValueAnimator.ofFloat(checkOffset, 1F)
-        } else {
-            checkedChangeValueAnimator = ValueAnimator.ofFloat(checkOffset, 0F)
-        }
+    private fun startBackgroundAnimation() {
+        checkedChangeValueAnimator?.cancel()
+        checkIconDrawOffsetValueAnimator?.cancel()
+
+        val endValue = if (isChecked) 1F else 0F
+        checkedChangeValueAnimator = ValueAnimator.ofFloat(checkOffset, endValue)
 
         checkedChangeValueAnimator?.addUpdateListener {
             checkOffset = it.animatedValue as Float
             invalidate()
         }
 
-        checkedChangeValueAnimator?.duration = 300
+        checkedChangeValueAnimator?.addListener(object : SimpleAnimatorListener() {
+            override fun onAnimationEnd(animation: Animator?) {
+                startIconDrawAnimation()
+            }
+        })
 
-        this.isChecked = checked
-
+        checkedChangeValueAnimator?.duration = 100
         checkedChangeValueAnimator?.start()
+
+    }
+
+    private fun startIconDrawAnimation() {
+        checkIconDrawOffsetValueAnimator?.cancel()
+        val endValue = if (isChecked) 0F else 1F
+
+        checkIconDrawOffsetValueAnimator = ValueAnimator.ofFloat(checkIconDrawOffset, endValue)
+
+        checkIconDrawOffsetValueAnimator?.addUpdateListener {
+            checkIconDrawOffset = it.animatedValue as Float
+            invalidate()
+        }
+        checkedChangeValueAnimator?.duration = 150
+
+        checkIconDrawOffsetValueAnimator?.start()
     }
 
     private fun measureSize(desiredSize: Int, measureSpec: Int): Int {
@@ -119,7 +162,22 @@ class AnimCheckBox @JvmOverloads constructor(
         borderPaint.strokeWidth = strokeWidth
         borderPaint.isAntiAlias = true
 
+        checkIconPaint.strokeWidth = 10F
+        checkIconPaint.style = Paint.Style.STROKE
+        checkIconPaint.color = checkIconColor
+        checkIconPaint.isAntiAlias = true
+
         fullArea.addCircle(radius, radius, radius, Path.Direction.CW)
+    }
+
+    private fun initCheckIconPath() {
+
+        //todo hardcoded values
+        checkIconPath.moveTo(centerPoint.x - 40, centerPoint.y)
+        checkIconPath.lineTo(centerPoint.x - 10, centerPoint.y + 30)
+        checkIconPath.lineTo((measuredWidth - 40).toFloat(), centerPoint.y - 40)
+
+        checIconPathMeasure.setPath(checkIconPath, false)
     }
 
     override fun isHardwareAccelerated(): Boolean {
---
+++ app/src/main/java/com/example/ivan/customviews/anim_check_box/SimpleAnimatorListener.kt
@@ -0,0 +1,18 @@
+package com.example.ivan.customviews.anim_check_box
+
+import android.animation.Animator
+
+open class SimpleAnimatorListener : Animator.AnimatorListener{
+    override fun onAnimationRepeat(animation: Animator?) {
+    }
+
+    override fun onAnimationEnd(animation: Animator?) {
+    }
+
+    override fun onAnimationCancel(animation: Animator?) {
+    }
+
+    override fun onAnimationStart(animation: Animator?) {
+    }
+
+}
